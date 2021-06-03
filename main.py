d8863f66f3ba2a37fc8f617afac2543a027c8578
Yisi Liu <>
2021-06-03 17:40:55 +0000

    Not much

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import random
 win = 0
+chose = 0
+rock = 0
+scissor = 0
+paper = 0
 
 while win<1:
+  choice = ['rock', 'paper', 'scissor']
   print("This is a rock, paper, scissor game.")
-  user_input = input("Chose rock, paper, or scissors: ")
+  user_input = str(input("Chose rock, paper, or scissor: "))
   print("You chose " + user_input)
-  choice = ['rock', 'paper', 'scissors']
-  oppen = random.randint(choice)
-  
+  oppen = random.choice(choice)
+  print("The computer chose: " + oppen)
+  if (oppen == rock) and (user_input == scissor):
+    print("The computer has won!")
+    print("You lost.")
+    win = 1
