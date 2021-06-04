3a895aab0ec9e1ea1170b15583d71dd05746d5cf
Yisi Liu <>
2021-06-04 18:10:05 +0000

    I finished

--- main.py
+++ main.py
@@ -16,3 +16,13 @@ while win<1:
     print("The computer has won!")
     print("You lost.")
     win = 1
+  elif (oppen == scissor) and (user_input == paper):
+    print("The computer won!")
+    print("You have lost.")
+    win = 1
+  elif (oppen == paper) and (user_input == rock):
+    print("You have lost. And the computer has won!")
+    win = 1
+  else:
+    print("You beat the computer! You have won!")
+    win = 1
